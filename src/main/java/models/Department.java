193b25c6764f3af1cb9353029aba3746a5244cc3
Steve-njeri <stevenjeri254@gmail.com>
2021-08-21 13:32:36 +0300

    added new variables and a constructor for department

--- src/main/java/models/Department.java
+++ src/main/java/models/Department.java
@@ -1,4 +1,15 @@
 package models;
 
 public class Department {
+    private int id;
+    private String name;
+    private String description;
+    private int number_employees;
+
+    public Department(String name, String description, int number_employees){
+
+        this.name= name;
+        this.description = description;
+        this.number_employees= number_employees;
+    }
 }
