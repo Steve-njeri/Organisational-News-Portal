196248cb08e29995dd8e8145a2ce0900672419f2
Steve-njeri <stevenjeri254@gmail.com>
2021-08-21 14:39:30 +0300

    added new variables on news class

--- src/main/java/models/News.java
+++ src/main/java/models/News.java
@@ -1,4 +1,19 @@
 package models;
 
+import java.sql.Timestamp;
+
 public class News {
+    private int id;
+    private int user_id;
+    private String title;
+    private String content;
+    private Timestamp postdate;
+
+    public News(int id, int userId, String title, String content, Timestamp postdate) {
+        this.id = id;
+        this.user_id = userId;
+        this.title = title;
+        this.content = content;
+        this.postdate = postdate;
+    }
 }
