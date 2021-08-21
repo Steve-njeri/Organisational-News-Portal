b682f157492bd70e417b4c43dcb1ecb629e74972
Steve-njeri <stevenjeri254@gmail.com>
2021-08-21 15:18:43 +0300

    completed test in News class

--- src/main/java/models/News.java
+++ src/main/java/models/News.java
@@ -1,6 +1,7 @@
 package models;
 
 import java.sql.Timestamp;
+import java.util.Objects;
 
 public class News {
     private int id;
@@ -9,11 +10,67 @@ public class News {
     private String content;
     private Timestamp postdate;
 
-    public News(int id, int userId, String title, String content, Timestamp postdate) {
-        this.id = id;
-        this.user_id = userId;
+    public News(int user_id, String title, String content, Timestamp postdate) {
+        this.user_id = user_id;
         this.title = title;
         this.content = content;
+        this.postdate = postdate;
+    }
+
+    public int getUser_id() {
+        return user_id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public Timestamp getPostdate() {
+        return postdate;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof News)) return false;
+        News news = (News) o;
+        return id == news.id &&
+                Objects.equals(user_id, news.user_id) &&
+                Objects.equals(title, news.title) &&
+                Objects.equals(content, news.content) &&
+                Objects.equals(postdate, news.postdate);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, user_id, title, content);
+    }
+
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public void setPostdate(Timestamp postdate) {
         this.postdate = postdate;
     }
 }
--- src/test/java/models/NewsTest.java
+++ src/test/java/models/NewsTest.java
@@ -1,7 +1,39 @@
 package models;
 
+import org.junit.Test;
+
+import java.sql.Timestamp;
+import java.util.Date;
+
 import static org.junit.Assert.*;
 
 public class NewsTest {
+
+    @Test
+    public void NewsInstantiatesCorrectly() throws Exception{
+        News news = setupNews();
+        assertTrue(news instanceof News);
+    }
+
+    @Test
+    public void NewsInstantiatesCorrectlyWith_Values() throws Exception{
+        News news = setupNews();
+        assertEquals(0012,news.getUser_id());
+        assertEquals("Sports",news.getTitle());
+        assertEquals("FA Cup, Qualification",news.getContent());
+        assertEquals(new Timestamp(new Date().getTime()),news.getPostdate());
+    }
+
+    @Test
+    public void setId() throws Exception{
+        News news = setupNews();
+        news.setId(7);
+        assertNotEquals(3,news.getId());
+    }
+
+    //helper
+    public News setupNews() {
+        return new News(0012,"Sports","FA Cup, Qualification", new Timestamp(new Date().getTime()));
+    }
 
 }
