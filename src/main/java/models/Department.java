2d898327549fa5517b165bc332026efc72567b1c
Steve-njeri <stevenjeri254@gmail.com>
2021-08-21 14:13:27 +0300

    completed test on department class

--- src/main/java/models/Department.java
+++ src/main/java/models/Department.java
@@ -1,5 +1,7 @@
 package models;
 
+import java.util.Objects;
+
 public class Department {
     private int id;
     private String name;
@@ -12,4 +14,41 @@ public class Department {
         this.description = description;
         this.number_employees= number_employees;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getNumber_employees() {
+        return number_employees;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Department)) return false;
+        Department department = (Department) o;
+        return id == department.id &&
+                Objects.equals(name, department.name) &&
+                Objects.equals(description, department.description) &&
+                Objects.equals(number_employees, department.number_employees);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, id, description, number_employees);
+    }
+
 }
--- src/test/java/models/DepartmentTest.java
+++ src/test/java/models/DepartmentTest.java
@@ -1,7 +1,35 @@
 package models;
 
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class DepartmentTest {
+
+    @Test
+    public void DepartmentInstantiatesCorrectly() throws Exception{
+        Department department =setupDepartment();
+        assertTrue(department instanceof Department);
+    }
+
+    @Test
+    public void Department_instantiatesWithCorrectValues() throws Exception{
+        Department department = setupDepartment();
+        assertEquals("sales", department.getName());
+        assertEquals("Marketing",department.getDescription());
+        assertEquals(40,department.getNumber_employees());
+    }
+
+    @Test
+    public void setId() throws Exception{
+        Department department = setupDepartment();
+        department.setId(3);
+        assertNotEquals(2,department.getId());
+    }
+
+    // helper
+    public Department setupDepartment() {
+        return new Department("sales", "Marketing", 40);
+    }
 
 }
