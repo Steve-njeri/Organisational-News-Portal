3dc50f1efd8132bf12fbe4094213088cae1f63f6
Steve-njeri <stevenjeri254@gmail.com>
2021-08-21 16:26:55 +0300

    added App.java class

--- src/main/java/models/DepartmentNews.java
+++ src/main/java/models/DepartmentNews.java
@@ -1,10 +1,31 @@
 package models;
 
 import java.sql.Timestamp;
+import java.util.Objects;
 
 public class DepartmentNews extends News {
+    private int departmentId;
 
-    public DepartmentNews(int user_id, String title, String content, Timestamp postdate) {
+    public DepartmentNews(int user_id, String title, String content, Timestamp postdate, int departmentId) {
+
         super(user_id, title, content, postdate);
+        this.departmentId = departmentId;
+    }
+
+    public int getDepartmentId() {
+        return departmentId;
+    }
+
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof News)) return false;
+        DepartmentNews departmentNews = (DepartmentNews) o;
+        return departmentId == departmentNews.departmentId;
+
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), departmentId);
     }
 }
--- src/test/java/models/DepartmentNewsTest.java
+++ src/test/java/models/DepartmentNewsTest.java
@@ -1,7 +1,43 @@
 package models;
 
+import org.junit.Test;
+
+import java.sql.Timestamp;
+import java.util.Date;
+
 import static org.junit.Assert.*;
 
 public class DepartmentNewsTest {
+
+    @Test
+    public void NewsInstantiatesCorrectly() throws Exception{
+        DepartmentNews departmentNews = setupDepartmentNews();
+        assertTrue(departmentNews instanceof DepartmentNews);
+    }
+
+    @Test
+    public void DepartmentNewsInstantiatesCorrectlyWith_Values() throws Exception{
+        DepartmentNews departmentNews = setupDepartmentNews();
+        assertEquals(0012,departmentNews.getUser_id());
+        assertEquals("Sports",departmentNews.getTitle());
+        assertEquals("FA Cup, Qualification",departmentNews.getContent());
+        assertEquals(new Timestamp(new Date().getTime()),departmentNews.getPostdate());
+        assertEquals(00111,departmentNews.getDepartmentId());
+
+    }
+
+    @Test
+    public void setId() throws Exception{
+        DepartmentNews departmentNews = setupDepartmentNews();
+        departmentNews.setId(7);
+        assertNotEquals(3,departmentNews.getId());
+    }
+
+    //helper
+    public DepartmentNews setupDepartmentNews() {
+        return new DepartmentNews(0012,"Sports","FA Cup, Qualification", new Timestamp(new Date().getTime()),00111);
+    }
+
+
 
 }
