393dfe8ef9c5466c29575eb42d4d28ee00c6f3e9
Steve-njeri <stevenjeri254@gmail.com>
2021-08-21 15:24:07 +0300

    added new variables on Users class

--- src/main/java/models/Users.java
+++ src/main/java/models/Users.java
@@ -1,5 +1,17 @@
 package models;
 
 public class Users {
+    private int id;
+    private String name;
+    private String position;
+    private String role;
+    private int departmentId;
+
+    public Users(String name, String position, String role, int departmentId) {
+        this.name = name;
+        this.position = position;
+        this.role = role;
+        this.departmentId = departmentId;
+    }
 
 }
